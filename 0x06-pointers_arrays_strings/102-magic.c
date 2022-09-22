6bdb7a122df3cf99a511671d6ef24d067429fc5c
SirClemz <clementanieto@gmail.com>
2022-09-22 13:52:47 -0700

    task 4 more pointers

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Prints a[2]=98 followed by a new line
  *
@@ -22,7 +23,7 @@ p = &n;
    * - you are not allowed to code anything else than this line of code
    */
 *(p + 5) = 98;
-/* ...so that this prints 98\n */
+  /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
 }
